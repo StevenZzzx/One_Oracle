04027e331d122f94c60a94d681f5ad6fc969d2df
StevenZzz <126300514+StevenZzzx@users.noreply.github.com>
2023-04-14 16:45:19 -0600

    Logica Programacion Update

--- Challenge #01/hola.py
+++ Challenge #01/hola.py
@@ -1,16 +1,41 @@
 
-principal_letters = ["a","e","i","o","u"]
-change_letters = ["ai","enter","imes","ober","ufat"]
+principal_letters = ["a", "e", "i", "o", "u"]
+change_letters = ["ai", "enter", "imes", "ober", "ufat"]
 
 texto = "gato"
-encrypted_text = ""
+text = "gaitober"
 
-for letra in texto:
-    if letra in principal_letters:
-            posicion = principal_letters.indexOf(letra)
+def encriptar(palabra): 
+    encrypted_text = ""
+    
+    for letra in palabra:
+        if letra in principal_letters:
+            posicion = principal_letters.index(letra)
             encrypted_text += change_letters[posicion]
-    else:
-        encrypted_text += letra
-        
-print(encrypted_text)
+        else:
+            encrypted_text += letra
+    return (encrypted_text)
+
+def desencriptar(palabra2):
+    dencrypted_text = ""
+    i = 0
+
+    while i < len(palabra2):
+        letra = palabra2[i]
+
+        if letra in principal_letters:
+            posicion = principal_letters.index(letra)
+            dencrypted_text += principal_letters[posicion]
+            i += len(change_letters[posicion])
+        else:
+            dencrypted_text += letra
+            i += 1 
+    return (dencrypted_text)
+
+
+print(encriptar(texto))
+
+print(desencriptar(text))
+
+
 
--- Challenge #01/main.js
+++ Challenge #01/main.js
@@ -6,20 +6,12 @@ change_letters = ["ai","enter","imes","ober","ufat"]
 var decrypted_text = "hola como esta"
 var encrypted_text = "hoberlai cobermober enterstai"
 
-// for (){
-    //     for (var j = 0; j < 4; j++){
-        //         if (texto[i] === principal_letters[j]){
-            //             encrypted_text = encrypted_text + change_letters[j] 
-            //         }
-            //     }
-            //     encrypted_text += texto
-            // }
-
 function encrypt(text){
     var encrypted = ""
 
     for (var i in text){
         var letra = text[i]
+
         if (principal_letters.includes(letra)){
             var posicion = principal_letters.indexOf(letra)
             encrypted += change_letters[posicion]
@@ -37,8 +29,8 @@ function desencrypt(text){
     var i = 0
 
     while (i < text.length){
-        
         var letra = text[i]
+
         if (principal_letters.includes(letra)){
             var posicion = principal_letters.indexOf(letra)
             desencriptado += principal_letters[posicion]
@@ -54,3 +46,4 @@ function desencrypt(text){
 
 console.log(encrypt(decrypted_text))
 console.log(desencrypt(encrypted_text))
+
